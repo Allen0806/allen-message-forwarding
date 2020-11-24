422d3b07f7da7e84280560d41d74ed82b465b850
allen <allen@localhost>
2020-11-24 16:19:30 +0800

    增加新功能

--- allen-message-forwarding-server/src/main/java/com/allen/message/forwarding/MessageForwardingApplication.java
+++ allen-message-forwarding-server/src/main/java/com/allen/message/forwarding/MessageForwardingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 消息转发系统启动类
@@ -19,6 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient // 仅适用于Eureka注册中心
 // @EnableDiscoveryClient // 适用于其他注册中心
 @EnableCaching
+@EnableScheduling // 开启Spring定时任务支持
 @ComponentScan(basePackages = "com.allen")
 @MapperScan(basePackages = "com.allen.**.dao")
 public class MessageForwardingApplication {
---
+++ allen-message-forwarding-service/src/main/java/com/allen/message/forwarding/process/job/MessageProcessSpringJob.java
@@ -0,0 +1,56 @@
+package com.allen.message.forwarding.process.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.allen.message.forwarding.process.service.MessageProcessService;
+
+/**
+ * 消息处理定时任务，基于Spring实现
+ * 
+ * @author Allen
+ * @date 2020年11月23日
+ * @since 1.0.0
+ */
+public class MessageProcessSpringJob {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessageProcessSpringJob.class);
+
+	/**
+	 * 消息处理服务
+	 */
+	@Autowired
+	private MessageProcessService messageProcessService;
+
+	/**
+	 * 消息转发重试任务，每5分钟执行一次
+	 * 
+	 */
+	@Scheduled(cron = "0 0/5 * * * ? *")
+	public void forwardingRetryJobHandler() {
+		LOGGER.info("消息转发重试任务开始执行");
+		try {
+			messageProcessService.retryForward();
+		} catch (Exception e) {
+			LOGGER.error("消息转发重试任务执行失败", e);
+		}
+		LOGGER.info("消息转发重试任务执行结束");
+	}
+
+	/**
+	 * 消息回调重试任务，每5分钟执行一次
+	 * 
+	 */
+	@Scheduled(cron = "0 0/5 * * * ? *")
+	public void callbackRetryJobHandler() {
+		LOGGER.info("消息回调重试任务开始执行");
+		try {
+			messageProcessService.retryCallback();
+		} catch (Exception e) {
+			LOGGER.error("消息回调重试任务执行失败", e);
+		}
+		LOGGER.info("消息回调重试任务执行结束");
+	}
+}
